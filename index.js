de3c4d2bf7b947a3e98bb3a1163742f684719e11
Will Franzen <wtfranzen@gmail.com>
2014-12-14 11:30:39 -0600

    Rewrite as async

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-var exec = require('exec-sync');
+var fs = require('fs');
+var uuid = require('uuid');
+var exec = require('child_process').exec;
 
-module.exports.toIconsetSync = function(inpt, out) {
-    return exec('iconutil --convert iconset "' + inpt + '" --output "' + out + '"');
+var TEMP_PATH = __dirname + '/tmp/';
+
+var rmdir = function(path) {
+    fs.readdir(path, function(err, files) {
+        if(err)
+            return;
+        var unlinked = 0;
+        files.forEach(function(file) {
+            fs.unlink(path + '/' + file, function(err, data) {
+                unlinked++;
+
+                if(unlinked == files.length) {
+                    fs.rmdir(path);
+                }
+            });
+        });
+    });
 };
 
-module.exports.toICNSSync = function(inpt, out) {
-    return exec('iconutil --convert icns "' + inpt + '" --output "' + out + '"');
+module.exports.toIconset = function(file, cb) {
+    var id = uuid.v1();
+
+    var outpath = TEMP_PATH + id + '.iconset';
+
+    exec('iconutil --convert iconset "' + file + '" --output "' + outpath + '"', function(err, stdout) {
+        if(err) {
+            cb('No ICNS file found!', undefined);
+            return;
+        }
+
+        fs.readdir(outpath, function(err, files) {
+            if(err) {
+                cb('An error occured while reading the iconset.');
+                return;
+            }
+
+            var readfiles = {};
+
+            files.forEach(function(file) {
+                fs.readFile(outpath + '/' + file, function(err, data) {
+                    readfiles[file] = data;
+
+                    if(Object.keys(readfiles).length == files.length) {
+                        cb(undefined, readfiles);
+                        rmdir(outpath);
+                    }
+                });
+            });
+        });
+    });
 };
